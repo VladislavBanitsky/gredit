32ff49310ca23003bf702a73072c800f11371a25
VladislavBanitsky <v.banitsckij2016@yandex.ru>
2025-03-06 18:58:50 +0300

    Добавлен фильтр "Старинная бумага"

--- index.js
+++ index.js
@@ -3,6 +3,53 @@ var img = new Image();
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d");
 var fileName = "";
+
+/* Загрузка изображения на холст */
+$("#upload-file").on("change", function(){
+    var file = document.querySelector('#upload-file').files[0];
+    var reader = new FileReader();
+    if (file) {
+        fileName = file.name;
+        reader.readAsDataURL(file);
+    }
+    reader.addEventListener("load", function () {
+        img = new Image();
+        img.src = reader.result;
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+            $("#canvas").removeAttr("data-caman-id");
+        }
+    }, false);
+});
+
+/* Скачивание отредактированного изображения */
+$('#download-btn').on('click', function (e) {
+    var fileExtension = fileName.slice(-4);
+    if (fileExtension == '.jpg' || fileExtension == '.png') {
+        var actualName = fileName.substring(0, fileName.length - 4);
+    }
+    download(canvas, actualName + '-edited.jpg');  // вызываем функцию для формирования файла
+});
+
+function download(canvas, filename) {
+    var  e;
+    var lnk = document.createElement('a');
+
+    lnk.download = filename;
+    lnk.href = canvas.toDataURL("image/jpeg", 0.8);
+
+    if (document.createEvent) {
+        e = document.createEvent("MouseEvents");
+        e.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        lnk.dispatchEvent(e);
+    }
+    else if (lnk.fireEvent) {
+        lnk.fireEvent("onclick");
+    }
+}
+
 /* Сохранение исходных параметров изображения */
 $(document).ready(function() {
     $('input[type=range]').change(applyFilters);
@@ -28,4 +75,10 @@ $(document).ready(function() {
         this.render();
     });
 
-    })
+    $('#oldpaper-btn').on('click', function (e) {
+    Caman('#canvas', img, function () {
+        this.oldpaper().render();
+    });
+});
+
+})
