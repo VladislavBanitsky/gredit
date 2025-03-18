fa10953be4e6c2595a404d521748141cd9030e5f
VladislavBanitsky <v.banitsckij2016@yandex.ru>
2025-03-18 16:31:13 +0300

    Попытка добавить Eng локализацию

---
+++ static/js/redirect.js
@@ -0,0 +1,9 @@
+$('#ruCheckBox').on('change', function(){
+    // переадресация
+    window.location.href = "index.html";
+});
+
+$('#engCheckBox').on('change', function(){
+    // переадресация
+    window.location.href = "index_eng.html";
+});
